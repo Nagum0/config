604a76239b08302b3e920ab8c78920773d4ba327
Nagum0 <xpteeth@gmail.com>
2024-03-26 17:56:57 +0100

    Started working on minimal mode.

---
+++ nvim/lua/xptee/init.lua
@@ -0,0 +1,11 @@
+require("xptee.lazy")
+require("xptee.remap")
+require("xptee.opt")
+
+-- COLORSCHEME:
+require("gruvbox").setup({
+    terminal_colors = true,
+    transparent_mode = true,
+})
+
+vim.cmd("colorscheme gruvbox")
---
+++ nvim/lua/xptee/remap.lua
@@ -0,0 +1,5 @@
+local vim = vim
+vim.g.mapleader = " "
+
+-- Open vim explorer:
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
