000d8a83638f2829c6b05ce3359ccc7ef60a048b
Nagum0 <xpteeth@gmail.com>
2025-08-22 16:33:18 +0200

    nvim-lsp configured

--- nvim/lua/zack/plugins/mason.lua
+++ nvim/lua/zack/plugins/mason.lua
@@ -5,6 +5,14 @@ return {
     },
     config = function()
         local mason = require("mason")
+        local mason_lspconfig = require("mason-lspconfig")
+
         mason.setup()
+
+        mason_lspconfig.setup({
+            ensure_installed = {
+                "lua_ls",
+            },
+        })
     end,
 }
